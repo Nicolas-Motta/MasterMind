f2213fb5f4e4b5ab711808af08a45e7fe0a6321e
NICOLAS MOTTA <nicolas.motta@marconirovereto.it>
2025-06-29 22:08:53 +0200

    feat: add BrowserRouter function

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { runWebSocketTest } from './test/WebSocketTester';
+import './main.css'; 
+import Lobby from './components/Lobby/Lobby';
 
 runWebSocketTest(); // Test WebSocket
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Lobby/>}/>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
