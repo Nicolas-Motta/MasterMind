37a3acd76b90d2142f78ef8c2a76483a6344f857
NICOLAS MOTTA <nicolas.motta@marconirovereto.it>
2025-07-01 00:39:37 +0200

    feat: implement Window component for resolve sometime using

--- frontend/src/components/CareerMenu/CareerMenu.tsx
+++ frontend/src/components/CareerMenu/CareerMenu.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "../Button/Button";
+import Window from "../Window/Window";
 import "./CareerMenu.css";
 import { useNavigate } from "react-router-dom";
 
@@ -11,11 +12,11 @@ export default function CareerMenu() {
     }
 
     return (
-        <div className="careerMenu">
+        <Window className="careerMenu">
             <h3>Career</h3>
             <Button className="gameButton" id="newButton" onClick={() => goToGame()}>NEW GAME</Button>
             <Button className="gameButton" id="continueButton">CONTINUE</Button>
             <Button className="gameButton" id="settingsButton">GAME SETTINGS</Button>
-        </div>
+        </Window>
     );
 }
--- frontend/src/components/OptionMenu/OptionMenu.tsx
+++ frontend/src/components/OptionMenu/OptionMenu.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Button from "../Button/Button";
+import Window from "../Window/Window";
 import "./OptionMenu.css";
 
 export default function OptionMenu() {
     return (
-        <div className="optionMenu">
+        <Window className="optionMenu">
             <h3>Option</h3>
             <Button className="optionButton" id="statsButton">STATS</Button>
             <Button className="optionButton" id="soundButton">SOUND</Button>
             <Button className="optionButton" id="grafichButton">GRAFICH</Button>
-        </div>
+        </Window>
     );
 }
