e5c743c67cf7b012ce57402967eca72e5fc6a57a
NICOLAS MOTTA <nicolas.motta@marconirovereto.it>
2025-08-13 13:19:42 +0200

    feat: add Ball component

---
+++ backend/src/main/java/com/backend/Ball.java
@@ -0,0 +1,41 @@
+package com.backend;
+
+import com.backend.Enums.*;
+
+public class Ball {
+    private String id;
+    private Color color;
+    private Position position;
+
+    public Ball(Color color, Position position) {
+        this.id =
+            "01" +
+            switch (color) {
+            case RED -> "0";
+            case BLUE -> "1";
+            case GREEN -> "2";
+            case YELLOW -> "3";
+            case ORANGE -> "4";
+            case PURPLE -> "5";
+            } +
+            (int)(Math.random() * 100);
+        this.color = color;
+        this.position = position;
+    }
+    
+    public Color getColor() {
+        return color;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+}
