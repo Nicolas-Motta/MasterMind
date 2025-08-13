db5e86b74374b5880b2d71225860a569ab8a48bb
NICOLAS MOTTA <nicolas.motta@marconirovereto.it>
2025-08-13 22:10:36 +0200

    feat: add OptionMenu component

---
+++ frontend/src/components/OptionMenu/OptionMenu.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "../Button/Button";
+import Window from "../Window/Window";
+import "./OptionMenu.css";
+
+export default function OptionMenu() {
+    return (
+        <Window className="optionMenu">
+            <h3>Option</h3>
+            <Button className="statsButton">STATS</Button>
+            <Button>SOUND</Button>
+            <Button>GRAFICH</Button>
+        </Window>
+    );
+}
