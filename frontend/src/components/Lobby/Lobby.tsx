ea991a8c62e38ba4cec3065cb0f11c5a7ea316a7
NICOLAS MOTTA <nicolas.motta@marconirovereto.it>
2025-08-13 22:39:16 +0200

    feat: implement Lobby component with menu toggling

--- frontend/src/components/Lobby/Lobby.tsx
+++ frontend/src/components/Lobby/Lobby.tsx
@@ -1,11 +1,42 @@
-
-
+import React, { useState } from "react"
+import Button from "../Button/Button"
+import CareerMenu from "../CareerMenu/CareerMenu"
+import OptionMenu from "../OptionMenu/OptionMenu"
 import "./Lobby.css"
 
 export default function Lobby() {
+    const [careertMenu, setCareertMenu] = useState<React.ReactNode | null>(null);
+    const [optionMenu, setOptionMenu] = useState<React.ReactNode | null>(null);
 
-  return (
-    <div>
-    </div>
-  );
+    function toggleCareertMenu(menuComponent: React.ReactNode) {
+        if (careertMenu) {
+            setCareertMenu(null);
+        } else {
+            setCareertMenu(menuComponent);
+            setOptionMenu(null);
+        }
+    }
+
+    function toggleOptionMenu(menuComponent: React.ReactNode) {
+        if (optionMenu) {
+            setOptionMenu(null);
+        } else {
+            setOptionMenu(menuComponent);
+            setCareertMenu(null);
+        }
+    }
+
+    return (
+        <div className="Lobby">
+            <h1>MasterMind</h1>
+            <div className="buttons">
+                <Button className="careerButton" onClick={() => toggleCareertMenu(<CareerMenu />)}>CAREER</Button>
+                <Button className="infinityButton">INFINITY</Button>
+                <Button className='quitButton'>QUIT</Button>
+                <Button className="optionButton" onClick={() => toggleOptionMenu(<OptionMenu />)}>OPTION</Button>
+            </div>
+            {careertMenu}
+            {optionMenu}
+        </div>
+    )
 }
