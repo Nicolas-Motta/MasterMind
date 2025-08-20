08dee733bd42ce751c0d229da0f8fc7d0a03851c
NICOLAS MOTTA <nicolas.motta@marconirovereto.it>
2025-08-20 11:36:22 +0200

    feat: add PositionType and add "ERROR" state

--- backend/src/main/java/com/backend/Enums/Position.java
+++ backend/src/main/java/com/backend/Enums/Position.java
@@ -2,11 +2,12 @@ package com.backend.Enums;
 
 public enum Position {
     HOME,
+    LABEL0,
     LABEL1,
     LABEL2,
     LABEL3,
     LABEL4,
     LABEL5,
-    LABEL6,
-    RESULT
+    RESULT,
+    ERROR
 }
---
+++ frontend/src/Types/Position.ts
@@ -0,0 +1,10 @@
+export type Position = 
+    | "HOME"
+    | "LABEL0"
+    | "LABEL1"
+    | "LABEL2"
+    | "LABEL3"
+    | "LABEL4"
+    | "LABEL5"
+    | "RESULT"
+    | "ERROR";
