c6a02ea7c01e11f22182f3bcc46c8f2e34eac9d1
NICOLAS MOTTA <nicolas.motta@marconirovereto.it>
2025-11-14 14:13:57 +0100

    feat: add InfinityPop component

---
+++ frontend/src/components/InfinityPop/InfinityPop.tsx
@@ -0,0 +1,12 @@
+import Window from "../Window/Window";
+import "./InfinityPop.css";
+
+export default function InfinityPop() {
+
+    return (
+        <Window className="infinityPopUp">
+            <h3>Infinity</h3>
+            <p>To unlock this feature buy the DLC, enroll at <span style={{color: "red"}}>M</span>arconi</p>
+        </Window>
+    )
+}
--- frontend/src/components/Lobby/Lobby.tsx
+++ frontend/src/components/Lobby/Lobby.tsx
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from "react"
 import Button from "../Button/Button"
 import CareerMenu from "../CareerMenu/CareerMenu"
 import OptionMenu from "../OptionMenu/OptionMenu"
+import InfinityPop from "../InfinityPop/InfinityPop"
 import { preloadComponents } from "../../utils/preloadUtils"
 import "./Lobby.css"
 
 export default function Lobby() {
     const [careertMenu, setCareertMenu] = useState<React.ReactNode | null>(null);
     const [optionMenu, setOptionMenu] = useState<React.ReactNode | null>(null);
+    const [infinityPop, setInfinityPop] = useState<React.ReactNode | null>(null);
 
     // Preload del componente Game al mount della Lobby
     useEffect(() => {
@@ -20,6 +22,7 @@ export default function Lobby() {
         } else {
             setCareertMenu(<CareerMenu />);
             setOptionMenu(null);
+            setInfinityPop(null);
         }
     }
 
@@ -29,6 +32,17 @@ export default function Lobby() {
         } else {
             setOptionMenu(<OptionMenu />);
             setCareertMenu(null);
+            setInfinityPop(null);
+        }
+    }
+
+    function toggleInfinityPop() {
+        if (infinityPop) {
+            setInfinityPop(null);
+        } else {
+            setInfinityPop(<InfinityPop />);
+            setCareertMenu(null);
+            setOptionMenu(null);
         }
     }
 
@@ -42,7 +56,11 @@ export default function Lobby() {
                 >
                     CAREER
                 </Button>
-                <Button className="infinityButton">INFINITY</Button>
+                <Button 
+                    className="infinityButton"
+                    onClick={() => toggleInfinityPop()}
+                    >
+                        INFINITY</Button>
                 <Button className='quitButton'>QUIT</Button>
                 <Button 
                     className="optionButton" 
@@ -53,6 +71,7 @@ export default function Lobby() {
             </div>
             {careertMenu}
             {optionMenu}
+            {infinityPop}
         </div>
     )
 }
